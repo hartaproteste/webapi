1270d6155109243f4090cf7b54617d0f1d2483da
Dan Bărbulescu <dan.barbulescu@hollo.software>
2017-02-02 23:53:22 +0200

    Validation

---
+++ logic/valid.js
@@ -0,0 +1,68 @@
+﻿var Validator = require('validatorjs');
+
+Validator.register('minVal', function (value, requirement) {
+  if (undefined === value) {
+    return true;
+  }
+
+  return (parseFloat(value) >= parseFloat(requirement));
+}, 'The :attribute must be at least :minVal.');
+
+Validator.register('maxVal', function (value, requirement) {
+  if (undefined === value) {
+    return true;
+  }
+  
+  return (parseFloat(value) <= parseFloat(requirement));
+}, 'The :attribute must be at most :maxVal.');
+
+
+Validator.register('domain', function (value) {
+  if (undefined === value) {
+    return true;
+  }
+
+  if ('string' != (typeof value)) {
+    return false;
+  }
+
+  if (value.length > 253) {
+    return false;
+  }
+
+  return !value.split('.').some(function (sub) {
+    if (!sub) {
+      return true;
+    }
+
+    if (sub.length > 63) {
+      return true;
+    }
+
+    if (!(/^[a-z\d](-*[a-z\d])*$/i.test(sub))) {
+      return true; 
+    }
+
+    return false;
+  });
+}, 'The :attribute is not valid.');
+
+Validator.Error = function _error() {
+  this.prototype.constructor.apply(this, Array.slice.apply(arguments));
+};
+
+Validator.Error.prototype   = new Error('Validation failed');
+Validator.Error.constructor = _error;
+
+module.exports = function (data, rules, context, errorClass) {
+  errorClass = errorClass || Validator.Error;
+  var validation = new Validator(data, rules);
+
+  if (validation.fails()) {
+    throw _.extend(new errorClass('Validation failed'), { errors: validation.errors, context: context });
+  }
+
+  return data;
+};
+
+module.exports.Error = Validator.Error;
--- logic/web.js
+++ logic/web.js
@@ -1,7 +1,30 @@
-﻿function _notAllowed(req, res) { res.send(new Response.MethodNotAllowed()); }
+﻿var valid = require('./valid);
+
+function _notAllowed(req, res) { res.send(new Response.MethodNotAllowed()); }
 
 module.exports = function (api) {
   api.get ('/', _notAllowed);
   
-  api.post('/', function (req, res) { res.send({ id: "0", lat: 44.452714, long: 26.085903, size: 250 }); });
+  api.post('/', function (req, res) {
+    
+    Promise.try(function () {
+      req.body = _.pick(req.body, _.negate(_.isEmpty));
+
+      utils.valid(req.body, {
+        uid       : 'required',
+        ts        : 'required|numeric',
+        lat       : 'required|numeric|minVal:-90|maxVal:90',
+        long      : 'required|numeric|minVal:-180|maxVal:180',
+        prec      : 'numeric'
+      });
+    }).then(function () {
+      res.send(new Response.OK({ id: '0', lat: 44.452714, long: 26.085903, size: 250 }));
+    }).catch(valid.Error, function (e) {
+      console.log(e);
+
+      throw new Response.BadRequest(_.values(_.values(e.errors)[0])[0][0]);
+    }).catch(function (err) {
+      next(e);
+    }).done();
+  });
 };
