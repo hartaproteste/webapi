15d15c170ff08f08c10201182615144049b68bec
Dan Bărbulescu <dan.barbulescu@ninesix.ro>
2017-02-02 21:39:16 +0200

    Initial skeleton

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# webapi
+﻿# webapi
+
+
---
+++ webapi.js
@@ -0,0 +1,12 @@
+﻿process.on('uncaughtException', function (err) {
+  console.error(new Date().toISOString(), 'FATAL', err);
+
+  process.exit(-1);
+});
+
+global.Promise = require('bluebird');
+global._       = require('underscore');
+
+global.config = require('./config.json');
+
+console.log(process.env.PGHOST);
