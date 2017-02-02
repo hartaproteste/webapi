9e08b52616d2fdc50b44bf2595f1e7b870c68dd6
Dan Bărbulescu <dan.barbulescu@hollo.software>
2017-02-03 00:37:38 +0200

    Fields

--- logic/web.js
+++ logic/web.js
@@ -36,11 +36,11 @@ module.exports = function (api) {
       req.body = _.pick(req.body, (v) => (!_.isEmpty(v) || _.isFinite(v)));
 
       valid(req.body, {
-        uid       : 'required',
-        ts        : 'required|numeric',
-        lat       : 'required|numeric|minVal:-90|maxVal:90',
-        long      : 'required|numeric|minVal:-180|maxVal:180',
-        prec      : 'numeric'
+        uid  : 'required',
+        ts   : 'required|numeric',
+        lat  : 'required|numeric|minVal:-90|maxVal:90',
+        lon  : 'required|numeric|minVal:-180|maxVal:180',
+        prec : 'numeric'
       });
     }).then(function () {
       res.send(new Response.OK({ id: '0', lat: 44.452714, long: 26.085903, size: 250 }));
