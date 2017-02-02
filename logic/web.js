e97fcba6749e3b2e98546698b1a98e717bbc2e06
Dan Bărbulescu <dan.barbulescu@hollo.software>
2017-02-02 23:26:27 +0200

    Mock data

--- logic/web.js
+++ logic/web.js
@@ -1,7 +1,7 @@
 ﻿function _notAllowed(req, res) { res.send(new Response.MethodNotAllowed()); }
 
 module.exports = function (api) {
-  api.get('/', _notAllowed);
+  api.get ('/', _notAllowed);
   
-  api.post('/test', function (req, res) { res.send(req.body); });
+  api.post('/', function (req, res) { res.send({ id: "0", lat: 44.452714, long: 26.085903, size: 250 }); });
 };
