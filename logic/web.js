65c86d31450039bd3752c9dfd78ca5bc0d0e9019
Dan Bărbulescu <dan.barbulescu@hollo.software>
2017-02-04 19:48:27 +0200

    Range

--- logic/web.js
+++ logic/web.js
@@ -84,7 +84,7 @@ module.exports = function (api) {
       return db.query([
             'SELECT f.type, f.name, f.extra, (',
                 'SELECT COUNT(DISTINCT t."uid") FROM "protest"."members" t WHERE t."fixed" = m."fixed"',
-                'AND date_trunc(\'minute\'::text, t."ts") = date_trunc(\'minute\'::text, to_timestamp($1) AT TIME ZONE \'UTC\')',
+                'AND date_trunc(\'minute\'::text, t."ts") >= date_trunc(\'minute\'::text, to_timestamp($1) AT TIME ZONE \'UTC\')',
               ') AS "total"',
             'FROM "protest"."members" m, "position"."fixed" f WHERE m.fixed = f.id AND m."uid" = $2',
             'AND date_trunc(\'minute\'::text, m."ts") = date_trunc(\'minute\'::text, to_timestamp($3) AT TIME ZONE \'UTC\')'
